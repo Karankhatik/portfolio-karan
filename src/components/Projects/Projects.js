c2f195980c87de7e7a9e6fab3a14ba86b2ef404b
Karan Khatik <karan13699@gmail.com>
2023-08-01 02:17:15 +0530

    Fix Bug

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,7 +8,6 @@ import justdabba from "../../Assets/Projects/justdabba1.png";
 import k_mail from "../../Assets/Projects/k-mail.png";
 import movie_app from "../../Assets/Projects/movie-app.png";
 
-
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -28,7 +27,6 @@ function Projects() {
               title="JustDabba"
               description="I worked with two team members to create a special website using NodeJS, React JS, Mongo DB (Atlas), and Express JS. Our website helps people who cook and sell Tiffin meals connect with customers who want to buy Tiffin meals nearby. Our project was the best among 40 teams in our semester!"
               ghLink="https://github.com/Karankhatik/tiffin_project"
-              
             />
           </Col>
 
@@ -49,7 +47,7 @@ function Projects() {
               title="Pokedex"
               description="I developed a fully responsive front-end application with React, Tailwind CSS, and Parcel, showcasing my passion for front-end development. My exceptional problem-solving skills were evident in how I effectively handled various API errors, ensuring a smooth user experience, while also incorporating visually appealing Shimmer effects to enhance the overall interface."
               ghLink="https://github.com/Karankhatik/Pokedex"
-              demoLink="https://pokedex-karan.vercel.app/"              
+              demoLink="https://pokedex-karan.vercel.app/"
             />
           </Col>
 
@@ -58,9 +56,8 @@ function Projects() {
               imgPath={movie_app}
               isBlog={false}
               title="Movie List"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
+              description="A responsive movie list web app built with React, Parcel bundler, and Bootstrap, allowing users to easily search for their favorite movies and book tickets hassle-free."
               ghLink="https://github.com/Karankhatik/movie-app"
-              
             />
           </Col>
 
@@ -69,9 +66,9 @@ function Projects() {
               imgPath={k_mail}
               isBlog={false}
               title="k-mail"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
+              description="
+              K-mail is a secure Gmail-inspired application developed using the MERN stack (MongoDB, Express, React, Node.js) and Material UI, implementing JWT authentication for enhanced security."
               ghLink="https://github.com/Karankhatik/k-mail-"
-              
             />
           </Col>
         </Row>
